43489db531e2f71fa3bbaa8ce9e2ae7735f4e8b1
k-sec-io <68455175+k-sec-io@users.noreply.github.com>
2020-09-29 00:50:55 +0800

    Add files via upload

---
+++ GpoInheritanceBlocked.ps1
@@ -0,0 +1,6 @@
+﻿$ous = Get-ADOrganizationalUnit -Filter 'Name -like "*"' 
+foreach ($ou in $ous)
+{
+    $ou_block_inheritance=Get-GPInheritance -Target $ou.DistinguishedName | where {$_.GpoInheritanceBlocked -eq "Yes"}
+    $ou_block_inheritance.Name
+}
---
+++ check_GPO_password_policy.ps1
@@ -0,0 +1,26 @@
+﻿$dest_nt_domain="mydomain.local"
+dir \\$dest_nt_domain\sysvol\$dest_nt_domain -I *.inf -R | Select-String MaximumPasswordAge
+dir \\$dest_nt_domain\sysvol\$dest_nt_domain -I *.inf -R | Select-String MinimumPasswordLength
+dir \\$dest_nt_domain\sysvol\$dest_nt_domain -I *.inf -R | Select-String PasswordComplexity
+dir \\$dest_nt_domain\sysvol\$dest_nt_domain -I *.inf -R | Select-String PasswordHistorySize
+dir \\$dest_nt_domain\sysvol\$dest_nt_domain -I *.inf -R | Select-String LockoutBadCount
+dir \\$dest_nt_domain\sysvol\$dest_nt_domain -I *.inf -R | Select-String ResetLockoutCount
+dir \\$dest_nt_domain\sysvol\$dest_nt_domain -I *.inf -R | Select-String LockoutDuration
+dir \\$dest_nt_domain\sysvol\$dest_nt_domain -I *.inf -R | Select-String RequireLogonToChangePassword
+dir \\$dest_nt_domain\sysvol\$dest_nt_domain -I *.inf -R | Select-String ForceLogoffWhenHourExpire
+dir \\$dest_nt_domain\sysvol\$dest_nt_domain -I *.inf -R | Select-String ClearTextPassword
+
+$gpo_folders = Get-ChildItem -Path \\$dest_nt_domain\sysvol\$dest_nt_domain\Policies\* | where { $_.PSIsContainer}
+foreach ($gpo_folder in $gpo_folders)
+{
+    Select-String -Path "$gpo_folder\MACHINE\Microsoft\Windows NT\SecEdit\GptTmpl.inf" -pattern MaximumPasswordAge
+    Select-String -Path "$gpo_folder\MACHINE\Microsoft\Windows NT\SecEdit\GptTmpl.inf" -pattern MinimumPasswordLength
+    Select-String -Path "$gpo_folder\MACHINE\Microsoft\Windows NT\SecEdit\GptTmpl.inf" -pattern PasswordComplexity
+    Select-String -Path "$gpo_folder\MACHINE\Microsoft\Windows NT\SecEdit\GptTmpl.inf" -pattern PasswordHistorySize
+    Select-String -Path "$gpo_folder\MACHINE\Microsoft\Windows NT\SecEdit\GptTmpl.inf" -pattern LockoutBadCount
+    Select-String -Path "$gpo_folder\MACHINE\Microsoft\Windows NT\SecEdit\GptTmpl.inf" -pattern ResetLockoutCount
+    Select-String -Path "$gpo_folder\MACHINE\Microsoft\Windows NT\SecEdit\GptTmpl.inf" -pattern LockoutDuration
+    Select-String -Path "$gpo_folder\MACHINE\Microsoft\Windows NT\SecEdit\GptTmpl.inf" -pattern RequireLogonToChangePassword
+    Select-String -Path "$gpo_folder\MACHINE\Microsoft\Windows NT\SecEdit\GptTmpl.inf" -pattern ForceLogoffWhenHourExpire
+    Select-String -Path "$gpo_folder\MACHINE\Microsoft\Windows NT\SecEdit\GptTmpl.inf" -pattern ClearTextPassword    
+}
